13c0ac52879281e154f748852cd0820d18fac54a
Agustin Nieva <agustin.n.dev@gmail.com>
2026-05-17 18:09:18 -0300

    refactor: detalle ingresos y path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindv4 from '@tailwindcss/vite';
+import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindv4()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
   },
 });
