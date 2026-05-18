22b579a3e2dd6f9097c8e9190af5d886ea19ae3f
Agustin Nieva <agustin.n.dev@gmail.com>
2026-05-18 01:02:50 -0300

    fear: nueva seccion de detalles de vehiculo

---
+++ src/types/vehicle.ts
@@ -0,0 +1,12 @@
+export interface Vehicle {
+	id: string;
+	image: string;
+	name: string;
+	price: string;
+	year: number;
+	mileage: string;
+	transmission: string;
+	fuel: string;
+	color: string;
+	description: string;
+}
